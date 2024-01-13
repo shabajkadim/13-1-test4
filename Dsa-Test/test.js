dcb48a178706ceca03ede46b40911ca6be9c7529
Shahabaj kadim <shahabajkadim7@gmail.com>
2024-01-13 17:02:42 +0530

    update

---
+++ Dsa-Test/test.js
@@ -0,0 +1,28 @@
+//Q.3
+// const isPalindrom=function(num){
+//     const str="" + num;
+    
+//     let left=0;
+//     let right=str.length-1;
+//     while(left<right){
+//         if(str[left]!==str[right]){
+//             return false;
+//         }else{
+//             left +=1
+//             right -=1
+//         }
+//     }
+//     return true
+// }
+// console.log(isPalindrom(121));
+
+//Q.7
+
+// const removeDublicate=function(num){
+//     return[...new Set(num)]
+// }
+// console.log(removeDublicate([1,2,3,4,5,4,3,2,5]))
+
+
+
+ 
