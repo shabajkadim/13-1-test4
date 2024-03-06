3ea88b3a8f6e822e23a1d385115ce4615676655b
Shahabaj kadim <shahabajkadim7@gmail.com>
2024-03-06 18:40:37 +0530

    updates

--- todo/src/component/TodoList.jsx
+++ todo/src/component/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './../style/todo.css'
 
 const TodoList = () => {
     const [activity,setActivity]=useState("");
@@ -9,31 +10,37 @@ const TodoList = () => {
         setListData((listData)=>{
             const updatelist=[...listData,activity]
             console.log(updatelist)
-            setActivity("")
+            setActivity("");
             return updatelist;
         })    
     }
     const RemoveAllList=(index)=>{
-        const removeall=listData.filter((elment,id)=>{
-            if (id =!index){
-
-            }
-            setListData([])
+        const removedata=listData.filter((element,id)=>{
+            return index != id
         })
+        setListData(removedata)
+    }
+
+    const removeAll=()=>{
+        setListData([])
     }
         
   return (
-    <div>
-      <div>
+    <div className='maindiv'>
+      <div className='childdiv'>
         <h1>MyTodo-List</h1>
         <div>
+            <div className='inputadd'>
+
             <input type='text' value={activity} onChange={(e)=>setActivity(e.target.value)} placeholder='Enter Your list'/>
             <button onClick={handleChange}>Add</button>
+
+            </div>
             <div>
                 {listData !=[] && listData.map((itom,index)=>{
                     return(
                         <>
-                        <div key={index}>
+                        <div key={index} className='inputdata'>
                         <div>{itom}</div>
                         <button onClick={()=>RemoveAllList(index)}>Remove</button>
                         </div>
@@ -42,6 +49,8 @@ const TodoList = () => {
 
                 })}
             </div>
+
+            {listData.length >=1 && <button onClick={removeAll}>Remove-All</button>}
         </div>
       </div>
 
